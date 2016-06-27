c5f33228e8422056a4c14da519f2adc9290579f9
DAndrey <andreydruzhaev@gmail.com>
2016-06-27 18:21:49 +0300

    Add outer model to subset regression

--- skl_ssr/subset_regression.py
+++ skl_ssr/subset_regression.py
@@ -65,16 +65,18 @@ class TTLinearRegression(BaseEstimator, RegressorMixin):
 
 class SubsetRegression(BaseEstimator, RegressorMixin):
 
-    def __init__(self, intercept=True, scorer=None, tol=None, verbose=False):
-        self.lr = None
-        self.intercept = intercept
+    def __init__(self, model_type=LinearRegression, model_params=None, scorer=None, tol=None, verbose=False):
         self.scorer = scorer
         self.tol = tol
         self.verbose = verbose
 
+        if model_params is None:
+            model_params = {}
+        self.model = model_type(**model_params)
+
     def _calc_lr_error(self, x, y):
-        self.lr.fit(x, y)
-        y_pred = self.lr.predict(x)
+        self.model.fit(x, y)
+        y_pred = self.model.predict(x)
         if self.scorer:
             return self.scorer(y, y_pred)
         return mean_squared_error(y, y_pred)
@@ -94,7 +96,7 @@ class SubsetRegression(BaseEstimator, RegressorMixin):
             best_subset.remove(var)
 
         is_converged = True
-        if best_var:
+        if best_var is not None:
             best_subset.add(best_var)
             is_converged = False
         return is_converged
@@ -112,15 +114,13 @@ class SubsetRegression(BaseEstimator, RegressorMixin):
                     best_err = new_err
                     worst_var = var
                 best_subset.add(var)
-            if worst_var:
+            if worst_var is not None:
                 best_subset.remove(worst_var)
                 worst_var = None
             else:
                 need_backward_step = False
 
     def fit(self, x, y):
-        self.lr = LinearRegression(fit_intercept=self.intercept)
-
         is_converged = False
         best_subset = set()
         vars_to_model = set(range(x.shape[1]))
@@ -137,10 +137,10 @@ class SubsetRegression(BaseEstimator, RegressorMixin):
                 msg = "Subset step. Number of features - {0}. Error value - {1}".format(len(best_subset), err)
                 print(msg)
         self.best_subset = best_subset
-        self.lr.fit(x[:, list(best_subset)], y)
+        self.model.fit(x[:, list(best_subset)], y)
 
     def predict(self, x):
-        return self.lr.predict(x)
+        return self.model.predict(x)
 
     def score(self, x, y, sample_weight=None):
         return self._calc_lr_error(x, y)
